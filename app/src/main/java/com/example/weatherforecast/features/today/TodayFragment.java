18c66924babdba1787e85aa12d1a94a4f3c39dfa
Anna <annaev0410@gmail.com>
2020-07-27 15:14:25 +0300

    Change the display of information (pre-prepared lines) and add a selection of images depending on weather conditions for TodayFragment

--- app/src/main/java/com/example/weatherforecast/features/today/TodayFragment.java
+++ app/src/main/java/com/example/weatherforecast/features/today/TodayFragment.java
@@ -10,9 +10,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.weatherforecast.R;
 import com.example.weatherforecast.databinding.FragmentTodayBinding;
 import com.example.weatherforecast.models.Current;
 import com.example.weatherforecast.models.Forecast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TodayFragment extends Fragment {
     private FragmentTodayBinding binding;
@@ -38,27 +42,121 @@ public class TodayFragment extends Fragment {
     private void loadList() {
         Forecast forecast = (Forecast) getArguments().getSerializable("forecast");
         Current current = forecast.getCurrent();
-        binding.tvTemperature.setText(String.valueOf(current.getTemp()));
+        binding.tvTemperature.setText(getString(R.string.temperature, (int) current.getTemp()));
         binding.tvDescriptionWeather.setText(current.getWeather().get(0).getDescription());
-        binding.tvTemperatureFeelsLike.setText(String.valueOf(current.getFeelsLike()));
-        binding.tvWindSpeed.setText(String.valueOf(current.getWindSpeed()));
-        binding.tvClouds.setText(String.valueOf(current.getClouds()));
-        binding.tvHumidity.setText(String.valueOf(current.getHumidity()));
-        binding.tvPressure.setText(String.valueOf(current.getPressure()));
-        binding.tvSunrise.setText(String.valueOf(current.getSunrise()));
-        binding.tvSunset.setText(String.valueOf(current.getSunset()));
+        binding.tvTemperatureFeelsLike.setText(getString(R.string.temperature_feels_like, (int) current.getFeelsLike()));
+        binding.tvWindSpeed.setText(getString(R.string.wind_speed, (int) current.getWindSpeed()));
+        binding.tvClouds.setText(getString(R.string.clouds, current.getClouds()));
+        binding.tvHumidity.setText(getString(R.string.humidity, current.getHumidity()));
+        binding.tvPressure.setText(getString(R.string.pressure, current.getPressure()));
+        binding.tvSunrise.setText(new SimpleDateFormat("HH:mm").format(new Date(current.getSunrise() * 1000L)));
+        binding.tvSunset.setText(new SimpleDateFormat("HH:mm").format(new Date(current.getSunset() * 1000L)));
         binding.tvUvi.setText(String.valueOf(current.getUvi()));
+        selectImage(current);
 
         TodayHourlyAdapter hourlyAdapter = new TodayHourlyAdapter(getActivity());
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
         binding.rvHourlyForecast.setLayoutManager(layoutManager);
         binding.rvHourlyForecast.setAdapter(hourlyAdapter);
-        hourlyAdapter.setList(forecast.getHourly());
+        hourlyAdapter.setList(forecast.getTodayHourly());
 
         TodayDailyAdapter dailyAdapter = new TodayDailyAdapter(getActivity());
         LinearLayoutManager nextLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
         binding.rvDailyForecast.setLayoutManager(nextLayoutManager);
         binding.rvDailyForecast.setAdapter(dailyAdapter);
-        dailyAdapter.setList(forecast.getDaily());
+        dailyAdapter.setList(forecast.getTodayDaily());
+    }
+
+    private void selectImage(Current current) {
+        int time = Integer.parseInt(new SimpleDateFormat("HH").format(new Date(current.getDate() * 1000L)));
+        switch (current.getWeather().get(0).getId()) {
+            case 200:
+            case 201:
+            case 202:
+            case 210:
+            case 211:
+            case 212:
+            case 221:
+            case 230:
+            case 231:
+            case 232:
+                binding.ivImageWeather.setImageResource(R.drawable.thunderstorm);
+                break;
+            case 300:
+            case 301:
+            case 302:
+            case 310:
+            case 311:
+            case 312:
+            case 313:
+            case 314:
+            case 321:
+            case 520:
+            case 521:
+            case 522:
+            case 531:
+                binding.ivImageWeather.setImageResource(R.drawable.shower_rain);
+                break;
+            case 500:
+            case 501:
+            case 502:
+            case 503:
+            case 504:
+                if (time >= 6 && time <= 18) {
+                    binding.ivImageWeather.setImageResource(R.drawable.d_rain);
+                } else {
+                    binding.ivImageWeather.setImageResource(R.drawable.n_rain);
+                }
+                break;
+            case 511:
+            case 600:
+            case 601:
+            case 602:
+            case 611:
+            case 612:
+            case 613:
+            case 615:
+            case 616:
+            case 620:
+            case 621:
+            case 622:
+                binding.ivImageWeather.setImageResource(R.drawable.show);
+                break;
+            case 701:
+            case 711:
+            case 721:
+            case 731:
+            case 741:
+            case 751:
+            case 761:
+            case 762:
+            case 771:
+            case 781:
+                binding.ivImageWeather.setImageResource(R.drawable.mist);
+                break;
+            case 800:
+                if (time >= 6 && time <= 18) {
+                    binding.ivImageWeather.setImageResource(R.drawable.d_clear_sky);
+                } else {
+                    binding.ivImageWeather.setImageResource(R.drawable.n_clear_sky);
+                }
+                break;
+            case 801:
+                if (time >= 6 && time <= 18) {
+                    binding.ivImageWeather.setImageResource(R.drawable.d_few_clouds);
+                } else {
+                    binding.ivImageWeather.setImageResource(R.drawable.n_few_clouds);
+                }
+                break;
+            case 803:
+                binding.ivImageWeather.setImageResource(R.drawable.broken_clouds);
+                break;
+            case 804:
+                binding.ivImageWeather.setImageResource(R.drawable.overcast_clouds);
+                break;
+            case 802:
+            default:
+                binding.ivImageWeather.setImageResource(R.drawable.scattered_clouds);
+        }
     }
 }
