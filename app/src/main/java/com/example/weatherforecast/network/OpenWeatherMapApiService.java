4154529e4a696a43d46b15d5b37dc6c0c19f9863
Anna <annaev0410@gmail.com>
2020-08-17 13:07:39 +0300

    Change returned for GET query type when changing from Call to Single

--- app/src/main/java/com/example/weatherforecast/network/OpenWeatherMapApiService.java
+++ app/src/main/java/com/example/weatherforecast/network/OpenWeatherMapApiService.java
@@ -2,7 +2,7 @@ package com.example.weatherforecast.network;
 
 import com.example.weatherforecast.models.Forecast;
 
-import retrofit2.Call;
+import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -17,5 +17,5 @@ public interface OpenWeatherMapApiService {
      */
 
     @GET("onecall?units=metric&appid=719394f46a7c5afb89a3aa717085e890&")
-    Call<Forecast> getForecast(@Query("lat") double lat, @Query("lon") double lon);
+    Single<Forecast> getForecast(@Query("lat") double lat, @Query("lon") double lon);
 }
