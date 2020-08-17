2a6218f4a072c1faec240724e4c2308412b79277
Anna <annaev0410@gmail.com>
2020-08-17 11:24:02 +0300

    Describe how the service API works

--- app/src/main/java/com/example/weatherforecast/network/OpenWeatherMapApiService.java
+++ app/src/main/java/com/example/weatherforecast/network/OpenWeatherMapApiService.java
@@ -7,6 +7,15 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface OpenWeatherMapApiService {
+    /**
+     * Request a weather forecast from the OpenWeatherMap API using the received coordinates
+     * (User enters name of city, and we use GeoCoder to enter the coordinates of this city. This is done because the API only works with coordinates)
+     *
+     * @param lat using "GeoCoder", we get latitude coordinates of city entered by user
+     * @param lon using "GeoCoder", we get longitude coordinates of city entered by user
+     * @return weather data at the specified coordinates, including hourly and weekly forecasts
+     */
+
     @GET("onecall?units=metric&appid=719394f46a7c5afb89a3aa717085e890&")
     Call<Forecast> getForecast(@Query("lat") double lat, @Query("lon") double lon);
 }
