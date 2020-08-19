f3fd5d3463e0cea946755ba26cd553054d70f5fa
Anna <annaev0410@gmail.com>
2020-08-19 11:37:52 +0300

    DI: mark constructor for the Dagger to make the Repository dependency in RequestUseCase, inject OpenWeatherMapApiService (removing its creation in onCreate method)

--- app/src/main/java/com/example/weatherforecast/features/main/ForecastRepository.java
+++ app/src/main/java/com/example/weatherforecast/features/main/ForecastRepository.java
@@ -2,14 +2,22 @@ package com.example.weatherforecast.features.main;
 
 import com.example.weatherforecast.models.Forecast;
 import com.example.weatherforecast.network.OpenWeatherMapApiService;
-import com.example.weatherforecast.network.RetrofitClientInstance;
+
+import javax.inject.Inject;
 
 import io.reactivex.rxjava3.core.Single;
 
 public class ForecastRepository implements Repository {
+    @Inject
+    OpenWeatherMapApiService service;
+
+    @Inject
+    public ForecastRepository(OpenWeatherMapApiService service) {
+        this.service = service;
+    }
+
     @Override
     public Single<Forecast> getForecast(double lat, double lon) {
-        OpenWeatherMapApiService apiService = RetrofitClientInstance.getRetrofitInstance().create(OpenWeatherMapApiService.class);
-        return apiService.getForecast(lat, lon);
+        return service.getForecast(lat, lon);
     }
 }
